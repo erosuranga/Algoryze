8b60da3356fbef2ea71ea1d47e3fe40734704628
jscrui <juandesndr@gmail.com>
2023-11-23 17:11:46 -0300

    updates on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,12 @@ export function Header() {
     <header className="relative z-50 flex-none lg:pt-11">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
-          <Logo className="h-12 w-auto text-slate-900" />
+          <Logo className="h-12 w-auto text-gray-100" />
+          {/* <div className='text-gray-50'>
+            ALGORIZE
+          </div> */}
         </div>
-        <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
+        <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-yellow-500 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <p>
               <time dateTime="2022-04-04">04</time>-
