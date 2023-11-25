92b4375193ae57c1bb2b2def66a0dea2f678616f
erosuranga <erosuranga@hotmail.com>
2023-11-25 12:36:09 -0300

    logo

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -62,7 +62,7 @@ export function Testimonials() {
           </p>
         </div>
         <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
-          <div className="-mt-8 flex-row sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
+          <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
             {testimonials.map((testimonial) => (
               <div
                 key={testimonial.author.handle}
