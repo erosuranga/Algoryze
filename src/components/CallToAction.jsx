80b92d9e0dbea3d2ca300ff09c2f07b503c34e40
erosuranga <erosuranga@hotmail.com>
2023-11-28 13:22:05 -0300

    counter-callToAction

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,26 +1,26 @@
 'use client'
 
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import Isotipo from '../images/logos/isotipo_golden.png'
 import { BackgroundImage } from '@/components/BackgroundImage'
 import { Container } from '@/components/Container'
 import MacbookTrading from '@/images/macbook_trading.png'
 import { Transition } from '@headlessui/react'
-
+import Counter from './counter'
 
 const AnimatedMacbook = () => {
-  const [isInViewport, setIsInViewport] = useState(false);
-  const elementRef = useRef(null);
+  const [isInViewport, setIsInViewport] = useState(false)
+  const elementRef = useRef(null)
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         // Check if the target element is in the viewport
         if (entries[0].isIntersecting) {
-          setIsInViewport(true);
+          setIsInViewport(true)
         } else {
-          setIsInViewport(false);
+          setIsInViewport(false)
         }
       },
       {
@@ -28,26 +28,25 @@ const AnimatedMacbook = () => {
         root: null,
         // 0% means the element becomes visible as soon as any part of it enters the viewport
         threshold: 0.5,
-      }
-    );
+      },
+    )
 
     // Start observing the target element
     if (elementRef.current) {
-      observer.observe(elementRef.current);
+      observer.observe(elementRef.current)
     }
 
     // Cleanup: stop observing when the component unmounts
     return () => {
       if (elementRef.current) {
-        observer.unobserve(elementRef.current);
+        observer.unobserve(elementRef.current)
       }
-    };
+    }
   }, [])
-
 
   return (
     <div ref={elementRef} className="relative">
-      <Transition.Root show={isInViewport} as={Fragment} >
+      <Transition.Root show={isInViewport} as={Fragment}>
         <Transition.Child
           as={Fragment}
           enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -57,29 +56,36 @@ const AnimatedMacbook = () => {
           leaveFrom="translate-x-0"
           leaveTo="translate-x-full"
         >
-          <Image src={MacbookTrading} alt="macbook" className="h-auto text-gray-100" />
-
+          <Image
+            src={MacbookTrading}
+            alt="macbook"
+            className="h-auto text-gray-100"
+          />
         </Transition.Child>
       </Transition.Root>
     </div>
   )
-
 }
 
-
 export function CallToAction() {
-
   return (
-    <section id="schedule" aria-label="Schedule" className="py-20 sm:py-32 h-screen grid grid-cols-2">
+    <section
+      id="schedule"
+      aria-label="Schedule"
+      className="grid h-screen grid-cols-2 py-20 sm:py-32"
+    >
       <Container className="relative z-10">
-
         {/** topbar */}
-        <div className='h-16 mb-10'>
-          <Image src={Isotipo} alt="isotipo" className="h-16 w-auto text-gray-100" />
+        <div className="mb-10 h-16">
+          <Image
+            src={Isotipo}
+            alt="isotipo"
+            className="h-16 w-auto text-gray-100"
+          />
         </div>
 
         {/** title */}
-        <div className='flex flex-col'>
+        <div className="flex flex-col">
           <h2 className="font-display text-7xl font-medium tracking-tighter text-gray-50 sm:text-9xl">
             TOP #1
           </h2>
@@ -92,19 +98,21 @@ export function CallToAction() {
             INSTITUCIONAL
           </span>
 
-
-          <span className="ml-2 mt-10 font-display text-4xl tracking-tight text-gray-50">
-            +10000 <span className='text-yellow-400'>USUARIOS</span>
+          <span className="ml-2 mt-10  font-display text-4xl tracking-tight text-gray-50">
+            <span
+              id="mySection"
+              className="inline-block align-top text-yellow-400"
+            >
+              <Counter />
+            </span>
           </span>
 
-          <div className='text-center'>
-            <button className="animate-bounce mt-20 px-8 py-4 bg-yellow-400 text-gray-900 font-semibold rounded-lg shadow-md hover:bg-yellow-300 transition duration-300">
+          <div className="text-center">
+            <button className="mt-20 animate-bounce rounded-lg bg-yellow-400 px-8 py-4 font-semibold text-gray-900 shadow-md transition duration-300 hover:bg-yellow-300">
               Suscribirme
             </button>
           </div>
-
         </div>
-
       </Container>
       <Container className="relative">
         <AnimatedMacbook />
