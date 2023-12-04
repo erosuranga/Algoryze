52b010eed907f6ea058ab37d1b56a09ae154842f
erosuranga <erosuranga@hotmail.com>
2023-12-04 11:38:47 -0300

    indicators- estructure

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,3 +1,5 @@
+import '../styles/colortext.css'
+
 const stats = [
   { label: 'Founded', value: '2021' },
   { label: 'Employees', value: '37' },
@@ -7,7 +9,7 @@ const stats = [
 
 export function Indicators() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-[#151823] py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-4">
@@ -66,14 +68,14 @@ export function Indicators() {
           </div>
           <div>
             <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
-              <p className="text-base font-semibold leading-7 text-indigo-600">
+              {/*   <p className="text-base font-semibold leading-7 text-indigo-600">
                 Company values
-              </p>
-              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              </p> */}
+              <h1 className="gradient-text mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
                 INDICATORS
               </h1>
-              {/*      <div className="max-w-xl">
-                <p className="mt-6">
+              <div className="max-w-xl">
+                <p className="mt-6 text-gray-50">
                   Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
                   risus enim. Mattis mauris semper sed amet vitae sed turpis id.
                   Id dolor praesent donec est. Odio penatibus risus viverra
@@ -81,7 +83,7 @@ export function Indicators() {
                   rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
                   mauris semper sed amet vitae sed turpis id.
                 </p>
-                <p className="mt-8">
+                {/*   <p className="mt-8">
                   Et vitae blandit facilisi magna lacus commodo. Vitae sapien
                   duis odio id et. Id blandit molestie auctor fermentum
                   dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
@@ -94,10 +96,10 @@ export function Indicators() {
                   dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
                   varius vulputate et ultrices hac adipiscing egestas. Iaculis
                   convallis ac tempor et ut. Ac lorem vel integer orci.
-                </p>
-              </div> */}
+                </p> */}
+              </div>
             </div>
-            <dl className="mt-10 grid grid-cols-2 gap-8 border-t border-gray-900/10 pt-10 sm:grid-cols-4">
+            {/*   <dl className="mt-10 grid grid-cols-2 gap-8 border-t border-gray-900/10 pt-10 sm:grid-cols-4">
               {stats.map((stat, statIdx) => (
                 <div key={statIdx}>
                   <dt className="text-sm font-semibold leading-6 text-gray-600">
@@ -108,7 +110,7 @@ export function Indicators() {
                   </dd>
                 </div>
               ))}
-            </dl>
+            </dl> */}
             <div className="mt-10 flex">
               <a
                 href="#"
