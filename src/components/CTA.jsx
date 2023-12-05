cb6e3c45ece52a21adc29c3740fbab46b9e6c958
erosuranga <erosuranga@hotmail.com>
2023-12-05 16:17:13 -0300

    cta

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
-import MacbookTrading from '@/images/macbook_trading.png'
-
 export function Cta() {
   return (
-    <div className="bg-[#151823]">
+    <div className=" bg-[#151823]">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="relative isolate overflow-hidden bg-[#151823] px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <svg
